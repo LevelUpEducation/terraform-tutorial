7dc64ce82a7d8ad3a7d8b1f593601850ea55ba31
stefanthorpe <stefan.thorpe@gmail.com>
2018-03-17 11:31:05 +0700

    Added additional meta demos

--- 2d_Resources/resources.tf
+++ 2d_Resources/resources.tf
@@ -8,10 +8,17 @@ resource "aws_instance" "frontend" {
   depends_on    = ["aws_instance.backend"]
   ami           = "ami-66506c1c"
   instance_type = "t2.micro"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_instance" "backend" {
   count         = 2
   ami           = "ami-66506c1c"
   instance_type = "t2.micro"
+  timeouts {
+    create = "60m"
+    delete = "2h"
+  }
 }
